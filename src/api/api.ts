2044305b596ee41de606a66ca9f1fae2bfabeae2
Mo2men-dev <momensameh5@gmail.com>
2024-07-18 16:52:21 +0300

    quick-fix: added auth token

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,14 +1,35 @@
 import axios from "axios";
 
 export const fetchAllConditions = async () => {
-	const API_URL = import.meta.env.VITE_API_URL as string;
-	const response = (await axios.get(`${API_URL}/conditions/all/symptoms`)).data;
+	const ENV = import.meta.env.VITE_ENV;
+	const API_URL =
+		ENV === "dev"
+			? "http://localhost:3000"
+			: (import.meta.env.VITE_API_URL as string);
+	const AUTH_TOKEN = import.meta.env.VITE_AUTH_TOKEN as string;
+	const response = (
+		await axios.get(`${API_URL}/conditions/all/symptoms`, {
+			headers: {
+				Authorization: AUTH_TOKEN,
+			},
+		})
+	).data;
 	return response;
 };
 
 export const fetchCondition = async (id: string) => {
-	const API_URL = import.meta.env.VITE_API_URL as string;
-	const response = (await axios.get(`${API_URL}/conditions/${id}/symptoms`))
-		.data;
+	const ENV = import.meta.env.VITE_ENV;
+	const API_URL =
+		ENV === "dev"
+			? "http://localhost:3000"
+			: (import.meta.env.VITE_API_URL as string);
+	const AUTH_TOKEN = import.meta.env.VITE_AUTH_TOKEN as string;
+	const response = (
+		await axios.get(`${API_URL}/conditions/${id}/symptoms`, {
+			headers: {
+				Authorization: AUTH_TOKEN,
+			},
+		})
+	).data;
 	return response;
 };
